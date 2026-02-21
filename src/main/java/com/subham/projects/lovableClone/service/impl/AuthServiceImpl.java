307f49dc5731992785b8a89293ec436b63dcb2ba
subham <sydb3604@gmail.com>
2026-02-21 19:19:09 +0530

    Configure WebSecurityConfig and User Signup

--- src/main/java/com/subham/projects/lovableClone/service/impl/AuthServiceImpl.java
+++ src/main/java/com/subham/projects/lovableClone/service/impl/AuthServiceImpl.java
@@ -3,14 +3,39 @@ package com.subham.projects.lovableClone.service.impl;
 import com.subham.projects.lovableClone.dto.auth.AuthResponse;
 import com.subham.projects.lovableClone.dto.auth.LoginRequest;
 import com.subham.projects.lovableClone.dto.auth.SignupRequest;
+import com.subham.projects.lovableClone.entity.User;
+import com.subham.projects.lovableClone.error.BadRequestException;
+import com.subham.projects.lovableClone.mapper.UserMapper;
+import com.subham.projects.lovableClone.repository.UserRepository;
 import com.subham.projects.lovableClone.service.AuthService;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Transactional
 public class AuthServiceImpl implements AuthService {
+
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+    UserMapper userMapper;
+
     @Override
     public AuthResponse signup(SignupRequest request) {
-        return null;
+        userRepository.findByUsername(request.username()).ifPresent(user -> {
+            throw new BadRequestException("User already exists with username: " + request.username());
+        });
+
+        User user = userMapper.toUserEntityFromSignupRequest(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+        userRepository.save(user);
+
+        return new AuthResponse("dummy", userMapper.toUserProfileResponseFromUser(user));
     }
 
     @Override
