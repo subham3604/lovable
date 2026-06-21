6f541bb129b7b6d87cfce2a52bbcfdc3069c099d
subham <sydv3604@gmail.com>
2026-06-21 11:41:47 +0530

    add frontend cors

--- src/main/java/com/subham/projects/lovableClone/config/CorsConfig.java
+++ src/main/java/com/subham/projects/lovableClone/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://genesis-five-livid.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
